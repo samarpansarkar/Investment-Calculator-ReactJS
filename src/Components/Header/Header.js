6c5e09321b96d3a91b33fdeb1ba09868c570e7b8
samarpan sarkar <samarpansarkar209@gmail.com>
2023-09-06 11:31:39 +0530

    css part is incomplete

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../../assets/investment-calculator-logo.png';
+import classes from './Header.css'; 
+
+
+const Header = () => {
+  return (
+    <div>
+      <header className={classes.header}>
+        <img src={logo} alt="logo" />
+        <h1>Investment Calculator</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header
